1c668bea6d3638a18aeee652d5ccd1e3e4f28b9f
Anton <antonymager@gmail.com>
2020-09-16 12:40:56 +0300

    practica 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-"use strict";
-const user = 'Ivan';
+const nomberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-alert(`Привет, ${user}`);
+const personalMovieDB = {
+    count: nomberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его', '');
+personalMovieDB.movies[a] = b;  
+personalMovieDB.movies[c] = d;    
+
+console.log(personalMovieDB);
